ffee89fdec84170264074536d3b065331306bb47
drisstafouzert <driss.tafouzert@gmail.com>
2021-01-11 23:49:40 +0000

    update server.js file

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/portfolio'));
 app.get('/*',function(req, res)
 {
-    req.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
 });
 app.listen(process.env.PORT || 8080);
