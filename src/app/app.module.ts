1e0e3dafb7c637b69b86ed722240ef409750f110
drisstafouzert <driss.tafouzert@gmail.com>
2021-01-14 16:37:40 +0000

    - Add, Nav-bar and First section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-
+import {AppRoutingModule} from './app-routing.module';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
 import { AppComponent } from './app.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ContentComponent } from './components/content/content.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientTestingModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  @ViewChild('logoLinkedin') logoLinkedin :ElementRef
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  linkedinLogoEnter(event)
+  {
+    console.log("event",event);
+    
+    event.target.src="/assets/images/linkedin-icon-hover-2.png"    
+  }
+  linkedinLogoLeave(event)
+  {
+    event.target.src="/assets/images/linkedin-icon.png"
+  }
+  githubLogoEnter(event)
+  {
+    event.target.src="/assets/images/github-icon-hover-2.png"    
+  }
+  githubLogoLeave(event)
+  {
+    event.target.src="/assets/images/github-icon.png"
+  }
+
+}
