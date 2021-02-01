3dac01e7120d82c4ff3b6f3df0d3a9545c66061e
drisstafouzert <driss.tafouzert@gmail.com>
2021-02-01 18:32:20 +0000

    - Make site web responsive for mobile

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -10,55 +10,35 @@ import { Observable } from 'rxjs';
 export class NavBarComponent implements OnInit {
 
   @ViewChild('logoLinkedin') logoLinkedin :ElementRef
+  collapsed:boolean=true
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
+  
   linkedinLogoEnter(event)
   {
     event.target.src="/assets/images/linkedin-icon-hover-2.png"    
   }
+  
   linkedinLogoLeave(event)
   {
     event.target.src="/assets/images/linkedin-icon.png"
   }
+  
   githubLogoEnter(event)
   {
     event.target.src="/assets/images/github-icon-hover-2.png"    
   }
+
   githubLogoLeave(event)
   {
     event.target.src="/assets/images/github-icon.png"
   }
-  clickNavItem(event)
+  
+  toggleCollapsed()
   {
-    let items=document.querySelector("nav-item")
-    items.classList.forEach(e=>
-      {
-        console.log(e);
-        
-      })
-  }
-  serviceSubreddit():Observable<any>
-  {
-    console.log("call ubreddit");
-    
-    return this.http.get("http://localhost:8080/api/subreddit");
-  }
-  getAllSubreddit()
-  {
-    console.log("call ubreddit 2");
-    this.serviceSubreddit().subscribe(
-      result=>{
-        console.log(result)
-        
-      },
-      error=>
-      {
-        console.log("ERROR",error);
-        
-      }
-    )
+    this.collapsed=!this.collapsed
   }
 
 }
