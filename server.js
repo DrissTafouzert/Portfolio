9bf55c940dfaaef9d8014cbeab2329eb661e0187
drisstafouzert <driss.tafouzert@gmail.com>
2021-02-15 17:55:32 +0000

    - Update server.js to gzipped files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(compression());
 app.use(express.static(__dirname + '/dist/Portfolio'));
 app.get('/*',function(req, res)
 {
