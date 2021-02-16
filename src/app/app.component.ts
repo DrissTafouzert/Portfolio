0e1891bfd485f2c75cda71e0db15b29d89e24353
drisstafouzert <driss.tafouzert@gmail.com>
2021-02-16 16:40:02 +0000

    - Add onLoading icon, to show while stie load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit
+{
   title = 'Portfolio';
+  constructor()
+  {
+  }
+  ngAfterViewInit(): void 
+  {
+   this.loadingEnd()
+  }
+  ngOnInit(): void 
+  {
+    document.getElementById('content').style.display='none'
+  }
+  loadingEnd()
+  {
+    document.getElementById('content').style.display='block'
+    document.getElementById('onloading').style.display='none'
+  }
+
+ 
 }
